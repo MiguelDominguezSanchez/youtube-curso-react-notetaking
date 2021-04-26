0224153891a5404070483c440d9fafe4ba8735d6
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-04-26 18:02:46 +0200

    4. Componentes

---
+++ react-basicos/src/App.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import logo from './logo.svg'
+import Componente from './components/Componente'
+import './App.css'
+import { Fragment } from 'react'
+
+function App() {
+	return (
+		<Fragment>
+			<div className='App'>
+				<section>
+					<br />
+					<br />
+					<br />
+					<br />
+					<br />
+					<br />
+					<br />
+					<br />
+					<br />
+					<br />
+					<Componente msg='Hola soy un Componente Funcional Expresado desde una prop' />
+				</section>
+				<header className='App-header'>
+					<img src={logo} className='App-logo' alt='logo' />
+
+					<p>
+						Edit <code>src/App.js</code> and save to reload.
+					</p>
+
+					<a
+						className='App-link'
+						href='https://reactjs.org'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						Learn React
+					</a>
+				</header>
+			</div>
+			<div></div>
+		</Fragment>
+	)
+}
+
+export default App
---
+++ react-basicos/src/components/Componente.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+// Componente funcional en formato de Función Expresada con arrow function
+const Componente = (props) => <h2>{props.msg}</h2>
+
+// Componente Funcional en formato de Función Declarada
+/*
+function Componente(props) {
+	return <h2>{props.msg}</h2>
+}
+*/
+
+// Componente Basado en Clase
+/*
+class Componente extends Component {
+	render() {
+		return <h2>{this.props.msg}</h2>
+	}
+}
+*/
+
+export default Componente
