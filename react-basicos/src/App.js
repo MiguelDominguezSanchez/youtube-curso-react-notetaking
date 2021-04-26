dd4ebaa67a047101ad6dca5b69be1815df38b51f
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-04-26 18:49:59 +0200

    5. Propiedades

--- react-basicos/src/App.js
+++ react-basicos/src/App.js
@@ -2,44 +2,47 @@ import React from 'react'
 import logo from './logo.svg'
 import Componente from './components/Componente'
 import './App.css'
-import { Fragment } from 'react'
+import Propiedades from './components/Propiedades'
 
 function App() {
 	return (
-		<Fragment>
-			<div className='App'>
+		<div className='App'>
+			<header className='App-header'>
 				<section>
-					<br />
-					<br />
-					<br />
-					<br />
-					<br />
-					<br />
-					<br />
-					<br />
-					<br />
-					<br />
+					<hr />
+					<Propiedades
+						cadena='Esto es una cadena de texto'
+						numero={19}
+						booleano={true}
+						arreglo={[1, 2, 3]}
+						objeto={{ nombre: 'Jon', correo: 'jonmircha@gmail.com' }}
+						funcion={(num) => num * num}
+						elementoReact={<i>Esto es un elemento React</i>}
+						componenteReact={
+							<Componente msg='Soy un componente pasado como prop' />
+						}
+					/>
+					<hr />
 					<Componente msg='Hola soy un Componente Funcional Expresado desde una prop' />
+					<hr />
 				</section>
-				<header className='App-header'>
-					<img src={logo} className='App-logo' alt='logo' />
 
-					<p>
-						Edit <code>src/App.js</code> and save to reload.
-					</p>
+				<img src={logo} className='App-logo' alt='logo' />
 
-					<a
-						className='App-link'
-						href='https://reactjs.org'
-						target='_blank'
-						rel='noopener noreferrer'
-					>
-						Learn React
-					</a>
-				</header>
-			</div>
-			<div></div>
-		</Fragment>
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+
+				<a
+					className='App-link'
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
 	)
 }
 
