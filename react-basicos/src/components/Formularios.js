3859687af95d0c73f06662853a1a75991fb9330a
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-05-22 19:36:03 +0200

    22. Formularios

--- react-basicos/src/components/Formularios.js
+++ react-basicos/src/components/Formularios.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from 'react'
 
+/*
 export default function Formularios() {
 	const [nombre, setNombre] = useState('')
 	const [sabor, setSabor] = useState('')
@@ -80,6 +81,106 @@ export default function Formularios() {
 					id='terminos'
 					name='terminos'
 					onChange={(e) => setTerminos(e.target.checked)}
+				/>
+				<br />
+				<input type='submit' />
+			</form>
+		</Fragment>
+	)
+}
+*/
+
+export default function Formularios() {
+	const [form, setForm] = useState({})
+
+	const handleChange = (e) => {
+		setForm({
+			...form,
+			[e.target.name]: e.target.value,
+		})
+	}
+
+	const handleChecked = (e) => {
+		setForm({
+			...form,
+			[e.target.name]: e.target.checked,
+		})
+	}
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		alert('El formulario se ha enviado')
+	}
+	return (
+		<Fragment>
+			<h2>Formularios</h2>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor='nombre'>Nombre</label>
+				<input
+					type='text'
+					id='nombre'
+					name='nombre'
+					value={form.nombre}
+					onChange={handleChange}
+				/>
+				<p>Elige tu sabor JS favorito</p>
+				<input
+					type='radio'
+					id='vanilla'
+					name='sabor'
+					value='vanilla'
+					onChange={handleChange}
+					defaultChecked
+				/>
+				<label htmlFor='vanilla'>Vanilla</label>
+				<input
+					type='radio'
+					id='react'
+					name='sabor'
+					value='react'
+					onChange={handleChange}
+				/>
+				<label htmlFor='react'>React</label>
+				<input
+					type='radio'
+					id='vue'
+					name='sabor'
+					value='vue'
+					onChange={handleChange}
+				/>
+				<label htmlFor='vue'>Vue</label>
+				<input
+					type='radio'
+					id='angular'
+					name='sabor'
+					value='angular'
+					onChange={handleChange}
+				/>
+				<label htmlFor='angular'>Angular</label>
+				<input
+					type='radio'
+					id='svelte'
+					name='sabor'
+					value='svelte'
+					onChange={handleChange}
+				/>
+				<label htmlFor='svelte'>Svelte</label>
+				<p>Elige tu lenguaje de programación favorito</p>
+				<select name='lenguaje' onChange={handleChange}>
+					<option value=''>– – –</option>
+					<option value='js'>Javascript</option>
+					<option value='php'>PHP</option>
+					<option value='py'>Python</option>
+					<option value='go'>GO</option>
+					<option value='Ruby'>Ruby</option>
+				</select>
+				<br />
+				<label htmlFor='terminos'>acepto términos y condiciones</label>
+				<input
+					type='checkbox'
+					id='terminos'
+					name='terminos'
+					onChange={handleChecked}
 				/>
 				<br />
 				<input type='submit' />
